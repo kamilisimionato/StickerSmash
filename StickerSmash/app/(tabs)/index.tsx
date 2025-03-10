11e08577845c850843103c5d8109250434e3663c
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-03-10 10:44:33 -0300

    feito imagepicker

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,17 +1,32 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("Você não selecionou nenhuma imagem!");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Escolher uma foto" />
+        <Button theme="primary" label="Escolher uma foto" />
         <Button label="Usar esta foto" />
       </View>
     </View>
--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -1,18 +1,41 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
     label: string;
-
+    theme?: 'primary';
 }
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+    if (theme === 'primary') {
+    return (
+        <View 
+        style={[
+        styles.buttonContainer, 
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }, 
+        ]}>
+            <Pressable style={[styles.button, { backgroundColor: "#fff" }]} onPress={() => alert('AHHHHHHHHHH')}
+            >
+            <FontAwesome 
+                name="picture-o" 
+                size={18} 
+                color='#2529e' 
+                style={styles.buttonIcon}
+                />
+            <Text style={[styles.buttonLabel, { color: "#2529e" }]}>
+                {label}
+            </Text>
+            </Pressable>
+        </View>
+        );
+    }
   return (
     <View style={styles.buttonContainer}>
         <Pressable style={styles.button} onPress={() => alert('AHHHHHHHHHH')}>
         <Text style={styles.buttonLabel}>{label}</Text>
         </Pressable>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -34,5 +57,9 @@ const styles = StyleSheet.create({
     buttonLabel: {
         color: "#fff",
         fontSize: 16,
-    }
-})
+    },
+    buttonIcon: {
+        paddingRight: 8,
+        
+    },
+});
