87641d8beb537fb3e61370a8d2eaff00d37bcb6f
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-04-03 11:09:08 -0300

    feito o troço

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -5,7 +5,9 @@ import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
-import EmojiPicker from "@/components/emojiPicker";
+import EmojiPicker from "@/components/EmojiPicker";
+import { type ImageSource } from "expo-image";
+import EmojiList from "@/components/EmojiList";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -13,6 +15,7 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -70,7 +73,7 @@ export default function Index() {
       </View>
       )}
     <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-  
+      <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
     </EmojiPicker>
     </View>
   );
