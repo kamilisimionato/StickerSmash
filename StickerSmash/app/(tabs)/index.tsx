78424d41d5744f37933521dadc4bcf3b8494d8f8
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-04-07 10:48:29 -0300

    feito mais troço

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import { type ImageSource } from "expo-image";
 import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -50,7 +51,12 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        <ImageViewer imgSource={PlaceholderImage} 
+        selectedImage={selectedImage} 
+        />
+        {pickedEmoji && 
+        <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        }
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
